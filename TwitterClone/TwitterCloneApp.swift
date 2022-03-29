2843004884ca6f40b5cced4e4adc90a53c7a5d46
berkbeleli <berkbeleli@gmail.com>
2022-03-29 15:44:11 +0300

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Berk Beleli on 2022-03-29.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
