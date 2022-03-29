841cb69f6e03aed4827b62c16eea35e0c0b08016
berkbeleli <berkbeleli@gmail.com>
2022-03-29 15:52:30 +0300

    Project Folders Setup

---
+++ TwitterClone/Core/Feed/View/FeedView.swift
@@ -0,0 +1,27 @@
+//
+//  FeedView.swift
+//  TwitterClone
+//
+//  Created by Berk Beleli on 2022-03-29.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        
+        ScrollView{
+            LazyVStack{
+                ForEach(0...20, id: \.self){ _ in
+                    Text("Tweet Row view")
+                }
+            }
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
