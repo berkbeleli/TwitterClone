cd7662313129fde8f681a11524c30ba88b15d1cd
berkbeleli <berkbeleli@gmail.com>
2022-03-29 19:06:06 +0300

    added main tab view bottom of the app

---
+++ TwitterClone/Core/Messsages/MessageView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageView.swift
+//  TwitterClone
+//
+//  Created by Berk Beleli on 2022-03-29.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text("Message View")
+    }
+}
+
+struct MessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageView()
+    }
+}
---
+++ TwitterClone/Core/Notifications/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  TwitterClone
+//
+//  Created by Berk Beleli on 2022-03-29.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Notification View")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
